605a42ccffabe961046ed15fc22778dd0f06f781
Felix <jaewoongcho93@gmail.com>
2023-01-18 17:59:27 +0900

    update rethink

--- SolvedAlgorithms/swea_high1_1_too_greedy.py
+++ SolvedAlgorithms/swea_high1_1_too_greedy.py
@@ -19,37 +19,39 @@ while i < N:
 del tmp1, tmp2
 print(case)
 # setup left right cases and count
-count = 0
-run = 0
-while case:
-    # call with weights
-    tmp1 = []
-    tmp2 = []
-    tmp3 = []
-    for i in case.pop():
-        tmp1 += [weights[i-1]]
-    
-    # setup initial properties
-    tmp2 = [tmp1.pop()]
-    tmp3 = [tmp2[:]]+[[-tmp2.pop()]]
-    # [1], [-1]
-    while tmp1:
-        tmp2 = tmp3[:]
-        tmp3 = []
-        a = tmp1.pop()
-        while tmp2:
-            # for i in range(len(tmp2)):
-            b = tmp2.pop()
-            tmp3 += [b+[a]] + [b+[-a]]
-    # check left right
+# count = 0
+# run = 0
 
-    for i in tmp3:
-        test_tmp = 0
-        run += 1
-        for j in i:
-            test_tmp += j 
-            if test_tmp < 0:
-                break
-            if j == i[len(i)-1] :
-                count += 1
-print(count)
+# while case:
+#     # call with weights
+#     print(count)
+#     tmp1 = []
+#     tmp2 = []
+#     tmp3 = []
+#     for i in case.pop():
+#         tmp1 += [weights[i-1]]
+    
+#     # setup initial properties
+#     tmp2 = [tmp1.pop()]
+#     tmp3 = [tmp2[:]]+[[-tmp2.pop()]]
+#     # [1], [-1]
+#     while tmp1:
+#         tmp2 = tmp3[:]
+#         tmp3 = []
+#         a = tmp1.pop()
+#         while tmp2:
+#             # for i in range(len(tmp2)):
+#             b = tmp2.pop()
+#             tmp3 += [b+[a]] + [b+[-a]]
+#     # check left right
+
+#     for i in tmp3:
+#         test_tmp = 0
+#         run += 1
+#         for j in i:
+#             test_tmp += j 
+#             if test_tmp < 0:
+#                 break
+#             if j == i[len(i)-1] :
+#                 count += 1
+# print(count)
