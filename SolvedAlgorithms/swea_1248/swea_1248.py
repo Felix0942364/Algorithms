b1caf1e1b1c78c38814a77ebb545aa527ff741e9
Felix <jaewoongcho93@gmail.com>
2023-01-25 17:01:57 +0900

    binary tree search function needs modification

--- SolvedAlgorithms/swea_1248/swea_1248.py
+++ SolvedAlgorithms/swea_1248/swea_1248.py
@@ -1,45 +1,60 @@
 import sys
-sys.stdin = open("SolvedAlgorithms\swea_1247\swea_1247_input.txt", "r")
+sys.stdin = open("SolvedAlgorithms\swea_1248\swea_1248_input.txt", "r")
 
-def build_dict(people, coordinates):
-    tmp_dict = dict()
-    case = [('start', coordinates.pop(0), coordinates.pop(0)), ('end', coordinates.pop(0), coordinates.pop(0))]
-    for idx in range(people):
-        case.append((idx, coordinates.pop(0), coordinates.pop(0)))
-    for key1, x1, y1 in case:
-        for key2, x2, y2 in case:
-            if not key1 in tmp_dict.keys():
-                tmp_dict[key1] = dict()
-            if not key2 in tmp_dict[key1].keys():
-                tmp_dict[key1][key2] = []
-            tmp_dict[key1][key2] = abs(x2 - x1) + abs(y2 - y1)
-    return tmp_dict
-    
-def find_min_path(people, coordinates):
-    case_dict = build_dict(people, coordinates)
-    res = 200 * people
-    value, route = 0, ['start']
-    queue = [(value, route)]
-    while queue:
-        value, route = queue.pop()
-        if sorted(route[1:]) == list(range(people)):
-            value += case_dict[route[-1]]['end']
-            if res > value:
-                res = value
-            continue
-        for idx in range(people):
-            if not idx in route:
-                if value < res:
-                    queue.append((value + case_dict[route[-1]][idx], route + [idx]))
-    return res
-    
-# shell to call input
+class Tree:
+    def __init__(self, key_):
+        self.key = key_
+        self.left = 0
+        self.right = 0
+
+    def build_tree(self, lst):
+        while lst:
+            parent_node = lst.pop(0)
+            child_node = lst.pop(0)
+            print('input : ', parent_node, child_node, lst)
+            self.insert(parent_node, child_node)
+            print(self.key, self.left, self.right)
+            if self.left:
+                print(self.left, self.left.key, self.left.left, self.left.right)
+            if self.right:
+                print(self.right, self.right.key, self.right.left, self.right.right)
+
+    def insert(self, parent, child):
+        tmp = self.search(parent)
+        if not tmp.left:
+            tmp.left = Tree(child)
+        elif not tmp.right:
+            tmp.right = Tree(child)
+        self = tmp
+
+    def PrintTree(self):
+        print(self.key, end=',')
+        if self.left:
+            self.left.PrintTree()
+        if self.right:
+            self.right.PrintTree()
+
+    def search(self, key_):
+        if self.key == key_:
+            return self
+        else:
+            if self.left:
+                self.left.search(key_)
+            if self.right:
+                self.right.search(key_)
+
+    def size(self):
+        l = self.left.size() if self.left else 0
+        r = self.right.size() if self.right else 0
+        return l + r + 1
+
 def solve(test_case):
-    people = int(input())
-    coordinates = list(map(int, input().split()))
-    print(f'#{test_case} {find_min_path(people, coordinates)}')
+    node, edge, pos_1, pos_2 = input().split()
+    lst = [1, 2, 1, 3, 2, 4, 2, 5, 3, 6, 3, 7]
+    tree = Tree(1)
+    built_tree = tree.build_tree(lst)
 
 T = int(input())
-T = 10
+T = 1
 for test_case in range(1, T + 1):
     solve(test_case)
