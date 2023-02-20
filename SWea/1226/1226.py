9e716dc53645ce73712245b60b68833093904004
YuJinUk <yujinuk5140@gmail.com>
2023-02-21 08:40:32 +0900

    1226 refactoring

--- SWea/1226/1226.py
+++ SWea/1226/1226.py
@@ -1,45 +1,42 @@
 import sys
+from collections import deque
 sys.stdin = open("1226_input.txt", "r")
 
 def call_map_array():
-    map_lst = list(input())
-    map_size = len(map_lst)
-    for i in range(map_size-1):
-        map_lst += str(input())
-    map_lst = [int(x) for x in map_lst]
+    map_lst = [list(map(int, input()))]
+    map_size = len(map_lst[0])
+    for _ in range(map_size-1):
+        map_lst += [list(map(int, input()))]
     return map_lst, map_size
 
 def find_position(val, array, size):
-    tmp = array.index(val)
-    x = tmp % size
-    y = tmp // size
-    return x, y
+    for idx, row in enumerate(array):
+        if val in row:
+            return row.index(val), idx
 
 def find_path(start, goal, array, size):
-    current_x, current_y = find_position(start, array, size)
-    goal_x, goal_y = find_position(goal, array, size)
-    queue = []
-    queue.append((current_x, current_y))
-    while queue:
-        current_x, current_y = queue.pop()
-        if array[(current_x+1) + current_y*size] == 0 or array[(current_x+1) + current_y*size] == goal:
-            queue.append((current_x+1, current_y))
-            array[(current_x+1) + current_y*size] = 4
-        if array[(current_x-1) + current_y*size] == 0 or array[(current_x-1) + current_y*size] == goal:
-            queue.append((current_x-1, current_y))
-            array[(current_x-1) + current_y*size] = 4
-        if array[current_x + (current_y+1)*size] == 0 or array[current_x + (current_y+1)*size] == goal:
-            queue.append((current_x, current_y+1))
-            array[current_x + (current_y+1)*size] = 4
-        if array[current_x + (current_y-1)*size] == 0 or array[current_x + (current_y-1)*size] == goal:
-            queue.append((current_x, current_y-1))
-            array[current_x + (current_y-1)*size] = 4
-        if (goal_x, goal_y) in queue:
+    start = find_position(start, array, size)
+    goal = find_position(goal, array, size)
+
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+
+    stack = deque()
+    stack.append(start)
+
+    while stack:
+        row, column = stack.pop()
+        if (row, column) == goal:
             return True
+        array[row][column] = 1
+        for i in range(4):
+            if 0 <= row+dr[i] < size and 0 <= column+dc[i] < size:
+                if array[row+dr[i]][column+dc[i]] != 1:
+                    stack.append((row+dr[i], column+dc[i]))
     return False
 
 def solve(start, goal):
-    (array, size) = call_map_array()
+    array, size = call_map_array()
     return find_path(start, goal, array, size)
 
 T = 10
