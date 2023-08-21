b1ffb0a783a021e45da210326a45b1d9a97dada0
felix0942364 <jaewoongcho93@gmail.com>
2023-08-21 16:26:49 +0900

    add annotations

--- programmers/kakao_intern_2020/click_keypad.cpp
+++ programmers/kakao_intern_2020/click_keypad.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 
 using namespace std;
-struct Pair {
+struct Pair { // structure for coordinates
     int row;
     int col;
 };
 
-vector<Pair> map {
+vector<Pair> map { // mapping number to coordinates
     {3,1}, 
     {0,0}, {0,1}, {0,2},
     {1,0}, {1,1}, {1,2},
@@ -16,23 +16,23 @@ vector<Pair> map {
 
 char getHand(int num, string hand, Pair &left, Pair &right) {
     Pair newpoint = map[num];
-    if (num == 1 || num == 4 || num == 7) {
+    if (num == 1 || num == 4 || num == 7) { // highest priority left hand 1,4,7
         left = newpoint;
         return 'L';
     }
-    else if (num == 3 || num == 6 || num == 9) {
+    else if (num == 3 || num == 6 || num == 9) { // highest priority right hand 3,6,9
         right = newpoint;
         return 'R';
     }
-    int range_left = abs(newpoint.row - left.row) + abs(newpoint.col - left.col);
+    int range_left = abs(newpoint.row - left.row) + abs(newpoint.col - left.col); // for manhattan distance
     int range_right = abs(newpoint.row - right.row) + abs(newpoint.col - right.col);
-    if (range_left < range_right) {
+    if (range_left < range_right) { // compare left right range
         left = newpoint;
         return 'L';
     } else if (range_left > range_right) {
         right = newpoint;
         return 'R';
-    } else if (hand == "left") {
+    } else if (hand == "left") { // left hander
         left = newpoint;
         return 'L';
     } else {
@@ -43,7 +43,7 @@ char getHand(int num, string hand, Pair &left, Pair &right) {
 
 string solution(vector<int> numbers, string hand) {
     string answer = "";
-    Pair left = {3,0};
+    Pair left = {3,0}; // initial coord
     Pair right = {3,2};
     for (int num: numbers) {
         answer.push_back(getHand(num, hand, left, right));
--- programmers/kakao_intern_2020/max_val.cpp
+++ programmers/kakao_intern_2020/max_val.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-vector<char> op_set {'+', '-', '*'};
+vector<char> op_set {'+', '-', '*'}; // used operands are + - *
 
-long long calculate(long long a, long long b, char op) {
+long long calculate(long long a, long long b, char op) { // calculate helper function
     if (op == '+')
         return a + b;
     else if (op == '-')
@@ -21,17 +21,18 @@ long long evaluateExpression(const string& expression, const vector<char>& ops) 
     vector<long long> values;
     vector<char> operators;
     size_t pos = 0;
-    while (pos < expression.size()) {
-        if (isdigit(expression[pos])) {
-            values.push_back(stoll(expression.substr(pos)));
-            pos += to_string(values.back()).size();
+    while (pos < expression.size()) { // decode expression to integer and operator
+        if (isdigit(expression[pos])) { 
+            values.push_back(stoll(expression.substr(pos))); // vector pushback, string to longlong, string substring
+            pos += to_string(values.back()).size(); // add last added value size
         }
         else {
-            operators.push_back(expression[pos]);
-            pos++;
+            operators.push_back(expression[pos]); 
+            pos++; // add 1 since single operation counts as 1 character
         }
     }
-    for (char op : ops) {
+
+    for (char op : ops) { // run each operation in the order as predefined permutation
         vector<long long> new_values;
         vector<char> new_operators;
         for (size_t i = 0; i < operators.size(); i++) {
@@ -53,8 +54,8 @@ long long evaluateExpression(const string& expression, const vector<char>& ops) 
 
 long long solution(string expression) {
     long long answer = 0;
-    int len = op_set.size();
-    sort(op_set.begin(), op_set.end());
+    int len = op_set.size(); // length of operator set
+    sort(op_set.begin(), op_set.end()); // sorting is needed in order for all permutation to work
     do {
         long long result = evaluateExpression(expression, op_set);
         answer = max(answer, result);
